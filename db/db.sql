231fbbb409e2fd43fbf9a14fdecc7ebb5aecc783
Corné Düring <corneduring@Cornes-MBP.home>
2022-08-18 13:58:58 +0200

    Changes to .gitignore made

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,8 @@
-DROP
-    TABLE IF EXISTS comments;
-DROP
-    TABLE IF EXISTS followers;
-DROP
-    TABLE IF EXISTS posts;
-DROP
-    TABLE IF EXISTS users;
+-- DROP
+--     TABLE IF EXISTS comments;
+-- DROP
+--     TABLE IF EXISTS followers;
+-- DROP
+--     TABLE IF EXISTS posts;
+-- DROP
+--     TABLE IF EXISTS users;
